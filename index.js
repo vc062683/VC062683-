1c4f84dfa4e2783800fb9e5a05f47fb05219f4ee
vc062683 <62022714+vc062683@users.noreply.github.com>
2020-03-10 10:00:23 -0500

    Add files via upload
    INIT COMMIT

---
+++ index.js
@@ -0,0 +1,16 @@
+var text =
+  "<p>- Sometimes as teenagers is difficult to sleep, because of personal problems or etc. so take melatonin pills:the melatonin pills are made naturally to give melatonin / liquid that produces sleep to your brain, therefore you can take up to 10 pills of melatonin</p>\
+<p>-you can take tea to sleep: At any nearby store or pharmacy you can find this product </p>\
+<p>- fear does not let you sleep: convince yourself that what does not exist for the day does not exist at night </p>\
+           <p>I hope this was  <b>useful</b>, and thanks for reading my website.</p>";
+
+function submit() {
+  document.getElementById("extra information").innerHTML = text;
+}
+
+var text2 =
+"<p> ask some of the firends in school</p><p> talk to the principal, ask to see the camera</p><p> talk to the counselor about this weird behavior</p>"
+
+function submits() {
+  document.getElementById("school related").innerHTML = text2;
+}
